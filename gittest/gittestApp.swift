4170c5777577c646da3544ab8b850f0a02b8e6b9
keluokeda <913918146@qq.com>
2024-03-05 15:35:01 +0800

    Initial Commit

---
+++ gittest/gittestApp.swift
@@ -0,0 +1,17 @@
+//
+//  gittestApp.swift
+//  gittest
+//
+//  Created by 余艳辉 on 2024/3/5.
+//
+
+import SwiftUI
+
+@main
+struct gittestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
